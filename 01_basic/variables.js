c4f8858be2b90e289e0fef962017a1aec155ff04
sanketparajuli <parajulisanket7@gmail.com>
2024-07-13 22:35:15 +0545

    learned about vaiables

---
+++ 01_basic/variables.js
@@ -0,0 +1,16 @@
+const accountID = 1234
+let accountEmail = "sanket@gmail.com"
+let accountPassword = "sanket"
+let accountCity = "Kathmandu"
+
+
+
+//its better not to use var to create a variable 
+
+console.log(accountID)
+
+accountEmail = "parajuli@gmail.com"
+accountPassword = "parajuli"
+accountCity = "Biratnagar"
+
+console.table([accountID,accountEmail,accountPassword,accountCity])
