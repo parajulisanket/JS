c17f8f24b91a11978c6e204e28983af5e49d7cd6
sanketparajuli <parajulisanket7@gmail.com>
2024-07-14 10:34:21 +0545

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,21 @@
+// console.log(3+3); console.log("sanket")
+
+let name = "sanket"
+let age = "19"
+let loggedIn = false
+
+console.table([ name, age, loggedIn])
+/*
+number => 2 to power 52
+bigint
+string => ""
+boolean => either true or false
+null => standalone value
+undefined => 
+symbole => unique
+object => 
+*/
+
+console.log(typeof "sanket"); //type string
+console.log(typeof undefined); // type undefined
+console.log(typeof null); // type object
