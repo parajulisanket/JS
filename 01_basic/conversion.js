e19bd2445e04af4462a6f1aa611ccca86c23b4e6
sanketparajuli <parajulisanket7@gmail.com>
2024-07-19 17:28:16 +0545

    datatype conversion

---
+++ 01_basic/conversion.js
@@ -0,0 +1,11 @@
+// data type conversion number to string
+
+let number = 33
+let name = "sanket"
+let newNumber = String(number)
+let newName = String(name)
+console.log(newNumber);
+console.log(newName);
+
+console.log( typeof newNumber);
+console.log( typeof newName);
