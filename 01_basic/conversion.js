0d047c97eacd31a707b771aab76690f90d3f4bc9
sanketparajuli <parajulisanket7@gmail.com>
2024-07-20 10:55:51 +0545

    added string to number conversion

--- 01_basic/conversion.js
+++ 01_basic/conversion.js
@@ -9,3 +9,15 @@ console.log(newName);
 
 console.log( typeof newNumber);
 console.log( typeof newName);
+
+
+// data type conversion string to number
+
+let sName = "sanket"
+let newSName = Number(sName)
+
+console.log(newSName);
+console.log(sName);
+
+console.log(typeof newSName);
+console.log(typeof sName);
