c5a488bbc5911a7517fd94d29a82ea1f0db3dc47
sanketparajuli <parajulisanket7@gmail.com>
2024-07-23 11:36:11 +0545

    object things added

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,15 @@
 console.log("sanket")
+
+// objects things
+const person = {name: 'sanket', age: '19', isMarried: false}
+console.log(person);
+
+let person1 = {...person, name: "samana"} /**doing this changes the name of the object */
+console.log(person1);
+
+
+const names = ['sanket','samana', 'santoshe']
+console.log(names);
+
+let names1 = [...names, 'kapil']
+console.log(names1);
