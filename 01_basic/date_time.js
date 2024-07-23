8f278a79224f93cbfa4f8bcf434f59c69cfcaaa4
sanketparajuli <parajulisanket7@gmail.com>
2024-07-23 14:16:22 +0545

    date and time

---
+++ 01_basic/date_time.js
@@ -0,0 +1,14 @@
+//  date and time basic
+
+const date = new Date()
+console.log(date.toString());
+console.log(date.toLocaleString());
+console.log(date.toDateString());
+console.log(typeof date);
+
+
+let myDate = new Date(2024,0,1)
+console.log(myDate.toDateString());
+
+console.log(date.getDay());
+console.log(date.getMonth());
